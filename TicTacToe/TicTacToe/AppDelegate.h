90701a60bd91fb5af89a5b45d5e67aed37b9baa4
Cate Huston <catehuston@gmail.com>
2015-02-12 15:18:32 +0100

    removing project auto created stuff

--- TicTacToe/TicTacToe/AppDelegate.h
+++ TicTacToe/TicTacToe/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  TicTacToe
-//
-//  Created by Cate Huston on 12/02/2015.
-//  Copyright (c) 2015 catehuston.com. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
