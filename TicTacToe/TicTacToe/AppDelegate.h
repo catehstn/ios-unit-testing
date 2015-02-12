4b42e47642e1ad6bcf7d3dd577e271e245b736fe
Cate Huston <catehuston@gmail.com>
2015-02-12 15:15:48 +0100

    initial Xcode project creation

---
+++ TicTacToe/TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Cate Huston on 12/02/2015.
+//  Copyright (c) 2015 catehuston.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Cate Huston on 12/02/2015.
+//  Copyright (c) 2015 catehuston.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
