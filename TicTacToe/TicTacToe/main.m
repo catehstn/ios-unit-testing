8fff47334e6a439b513736f8757f8d872a3ec0fb
Cate Huston <catehuston@gmail.com>
2015-02-12 16:15:22 +0100

    removing comments from file

--- TicTacToe/TicTacToe/main.m
+++ TicTacToe/TicTacToe/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  TicTacToe
-//
-//  Created by Cate Huston on 12/02/2015.
-//  Copyright (c) 2015 catehuston.com. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
