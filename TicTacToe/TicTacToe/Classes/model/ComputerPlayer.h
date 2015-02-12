1e723540f7725107335ef264d93e835b6f48db40
Cate Huston <catehuston@gmail.com>
2015-02-12 17:23:07 +0100

    adding computer board class

---
+++ TicTacToe/TicTacToe/Classes/model/ComputerPlayer.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+@class TicTacToeBoard;
+
+@interface ComputerPlayer : NSObject
+
+- (id)initWithBoard:(TicTacToeBoard)board;
+
+@end
---
+++ TicTacToe/TicTacToe/Classes/model/ComputerPlayer.m
@@ -0,0 +1,19 @@
+#import "ComputerPlayer.h"
+
+#import "TicTacToeBoard.h"
+
+@interface ComputerPlayer ()
+  TicTacToeBoard board_;
+@end
+
+@implementation ComputerPlayer
+
+- (id)initWithBoard:(TicTacToeBoard)board {
+  self = [super init];
+  if (self) {
+    board_ = board;
+  }
+  return self;
+}
+
+@end
