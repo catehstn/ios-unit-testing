c183e537a52f91f12e5fc9355da05f42e3e6fdf8
Cate Huston <catehuston@gmail.com>
2015-02-18 15:11:36 +0100

    adding targets to buttons

--- TicTacToe/TicTacToe/Classes/presenters/HomePresenter.m
+++ TicTacToe/TicTacToe/Classes/presenters/HomePresenter.m
@@ -1,9 +1,14 @@
 #import "HomePresenter.h"
 
+#import "HomeView.h"
 #import "HomeViewController.h"
 
 @interface HomePresenter ()
 @property(nonatomic, readonly) HomeViewController *homeViewController;
+
+- (void)oButtonPressed:(id)sender;
+- (void)xButtonPressed:(id)sender;
+- (void)xoButtonPressed:(id)sender;
 @end
 
 @implementation HomePresenter
@@ -14,6 +19,36 @@
 
 - (HomeViewController *)homeViewController {
   return (HomeViewController *) [self viewController];
+}
+
+- (void)viewLoaded {
+  HomeView *view = [[self homeViewController] homeView];
+
+  [[view playOButton] addTarget:self
+                         action:@selector(oButtonPressed:)
+               forControlEvents:UIControlEventTouchUpInside];
+  [[view playXButton] addTarget:self
+                        action:@selector(xButtonPressed:)
+              forControlEvents:UIControlEventTouchUpInside];
+  [[view playXOButton] addTarget:self
+                          action:@selector(xoButtonPressed:)
+                forControlEvents:UIControlEventTouchUpInside];
+
+}
+
+- (void)oButtonPressed:(id)sender {
+  // TODO(cate): Fill this in.
+  NSLog(@"o button pressed");
+}
+
+- (void)xButtonPressed:(id)sender {
+  // TODO(cate): Fill this in.
+  NSLog(@"x button pressed");
+}
+
+- (void)xoButtonPressed:(id)sender {
+  // TODO(cate): Fill this in.
+  NSLog(@"xo button pressed");
 }
 
 @end
