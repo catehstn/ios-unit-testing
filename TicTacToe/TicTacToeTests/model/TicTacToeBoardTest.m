54b429194c05cc64256953d59a73d96aef4bb656
Cate Huston <catehuston@gmail.com>
2015-02-18 11:00:51 +0100

    add tests for X and O winning

--- TicTacToe/TicTacToeTests/model/TicTacToeBoardTest.m
+++ TicTacToe/TicTacToeTests/model/TicTacToeBoardTest.m
@@ -123,4 +123,48 @@
   XCTAssertEqual([board_ gameState], TicTacToeGameStateBoardFull);
 }
 
+- (void)testGameStateOWins {
+  /*
+   O | X | O
+   X | O | _
+   O | _ | X
+   */
+  XCTAssertTrue([board_ playXPos:0 yPos:0 toState:TicTacToeStateO]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:0 yPos:1 toState:TicTacToeStateX]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:0 yPos:2 toState:TicTacToeStateO]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:1 yPos:0 toState:TicTacToeStateX]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:1 yPos:1 toState:TicTacToeStateO]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:2 yPos:2 toState:TicTacToeStateX]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:2 yPos:0 toState:TicTacToeStateO]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateOWin);
+}
+
+- (void)testGameStateXWins {
+  /*
+   X | O | X
+   O | X | _
+   X | _ | O
+   */
+  XCTAssertTrue([board_ playXPos:0 yPos:0 toState:TicTacToeStateX]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:0 yPos:1 toState:TicTacToeStateO]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:0 yPos:2 toState:TicTacToeStateX]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:1 yPos:0 toState:TicTacToeStateO]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:1 yPos:1 toState:TicTacToeStateX]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:2 yPos:2 toState:TicTacToeStateO]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateNotEnded);
+  XCTAssertTrue([board_ playXPos:2 yPos:0 toState:TicTacToeStateX]);
+  XCTAssertEqual([board_ gameState], TicTacToeGameStateXWin);
+}
+
 @end
