5ac8213b4cf82ef33a7d43b28917bab88d959449
Cate Huston <catehuston@gmail.com>
2015-02-18 16:06:34 +0100

    add button pressed fn

--- TicTacToe/TicTacToe/Classes/presenters/GamePresenter.m
+++ TicTacToe/TicTacToe/Classes/presenters/GamePresenter.m
@@ -1,9 +1,12 @@
 #import "GamePresenter.h"
 
+#import "GameView.h"
 #import "GameViewController.h"
+#import "TicTacToeButton.h"
 
 @interface GamePresenter ()
 @property(nonatomic, readonly) GameViewController *gameViewController;
+- (void)buttonPressed:(id)sender;
 @end
 
 @implementation GamePresenter
@@ -15,6 +18,19 @@
 
 - (GameViewController *)gameViewController {
   return (GameViewController *) [self viewController];
+}
+
+- (void)viewLoaded {
+  for (TicTacToeButton *button in [[[self gameViewController] gameView] buttons]) {
+    [button addTarget:self
+               action:@selector(buttonPressed:)
+     forControlEvents:UIControlEventTouchUpInside];
+  }
+}
+
+- (void)buttonPressed:(id)sender {
+  TicTacToeButton *button = (TicTacToeButton *)sender;
+  NSLog(@"Button pressed at %d,%d", [button x], [button y]);
 }
 
 @end
