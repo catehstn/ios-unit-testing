47b0148fd6e643c0227d3d0ea0c8481101e536c7
Cate Huston <catehuston@gmail.com>
2015-02-18 16:03:38 +0100

    adding gameVc property to presenter

--- TicTacToe/TicTacToe/Classes/presenters/GamePresenter.m
+++ TicTacToe/TicTacToe/Classes/presenters/GamePresenter.m
@@ -2,11 +2,19 @@
 
 #import "GameViewController.h"
 
+@interface GamePresenter ()
+@property(nonatomic, readonly) GameViewController *gameViewController;
+@end
+
 @implementation GamePresenter
 
 + (GameViewController *)createViewController {
   // TODO(cate): Add options.
   return [[GameViewController alloc] initWithPresenter:[GamePresenter new]];
+}
+
+- (GameViewController *)gameViewController {
+  return (GameViewController *) [self viewController];
 }
 
 @end
