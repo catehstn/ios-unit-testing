c1b6677131436333ff53cb320716cc28dafc5f52
Cate Huston <catehuston@gmail.com>
2015-02-18 15:03:35 +0100

    adding homeViewController property

--- TicTacToe/TicTacToe/Classes/presenters/HomePresenter.m
+++ TicTacToe/TicTacToe/Classes/presenters/HomePresenter.m
@@ -2,10 +2,18 @@
 
 #import "HomeViewController.h"
 
+@interface HomePresenter ()
+@property(nonatomic, readonly) HomeViewController *homeViewController;
+@end
+
 @implementation HomePresenter
 
 + (HomeViewController *)createViewController {
   return [[HomeViewController alloc] initWithPresenter:[HomePresenter new]];
+}
+
+- (HomeViewController *)homeViewController {
+  return (HomeViewController *) [self viewController];
 }
 
 @end
