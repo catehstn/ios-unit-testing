8409e6845059ecb7a17f7cbd2359b557223144ab
Cate Huston <catehuston@gmail.com>
2015-02-19 11:55:48 +0100

    adding board to presenter

--- TicTacToe/TicTacToe/Classes/presenters/GamePresenter.m
+++ TicTacToe/TicTacToe/Classes/presenters/GamePresenter.m
@@ -2,10 +2,16 @@
 
 #import "GameView.h"
 #import "GameViewController.h"
+#import "TicTacToeBoard.h"
 #import "TicTacToeButton.h"
 
-@interface GamePresenter ()
+@interface GamePresenter () {
+  TicTacToeBoard *board_;
+}
+
 @property(nonatomic, readonly) GameViewController *gameViewController;
+
+- (id)initWithBoard:(TicTacToeBoard *)board;
 - (void)buttonPressed:(id)sender;
 @end
 
@@ -13,7 +19,17 @@
 
 + (GameViewController *)createViewController {
   // TODO(cate): Add options.
-  return [[GameViewController alloc] initWithPresenter:[GamePresenter new]];
+  TicTacToeBoard *board = [TicTacToeBoard new];
+  GamePresenter *presenter = [[GamePresenter alloc] initWithBoard:board];
+  return [[GameViewController alloc] initWithPresenter:presenter];
+}
+
+- (id)initWithBoard:(TicTacToeBoard *)board {
+  self = [super init];
+  if (self) {
+    board_ = board;
+  }
+  return self;
 }
 
 - (GameViewController *)gameViewController {
