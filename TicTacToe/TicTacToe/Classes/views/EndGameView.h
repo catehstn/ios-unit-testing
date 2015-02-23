fc322aa1d8cf0ee0309239d95cee4368b9ad0f11
Cate Huston <catehuston@gmail.com>
2015-02-23 08:57:54 +0100

    deleting comment

--- TicTacToe/TicTacToe/Classes/views/EndGameView.h
+++ TicTacToe/TicTacToe/Classes/views/EndGameView.h
@@ -1,11 +1,3 @@
-//
-//  EndGameView.h
-//  TicTacToe
-//
-//  Created by Cate Huston on 23/02/2015.
-//  Copyright (c) 2015 catehuston.com. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface EndGameView : UIView
