c252a992a99e885f27c65f8584b039ad8ebd8537
Cate Huston <catehuston@gmail.com>
2015-02-24 11:46:21 +0100

    adding completing 3 logic and tests (some failing)

--- TicTacToe/TicTacToe/Classes/model/ComputerPlayer.m
+++ TicTacToe/TicTacToe/Classes/model/ComputerPlayer.m
@@ -4,6 +4,14 @@
   TicTacToeBoard *board_;
   TicTacToeStateType type_;
 }
+- (BOOL)complete3IfPossibleForType:(TicTacToeStateType)type;
+- (BOOL)completeColumnIfPossible:(int)row type:(TicTacToeStateType)type;
+- (BOOL)completeRowIfPossible:(int)row type:(TicTacToeStateType)type;
+- (BOOL)checkState:(TicTacToeStateType)state
+        forSquareA:(TicTacToeStateType)sqA
+           squareB:(TicTacToeStateType)sqB
+           squareC:(TicTacToeStateType)sqC;
+- (void)playXPos:(int)x yPos:(int)y;
 @end
 
 @implementation ComputerPlayer
@@ -17,17 +25,112 @@
   return self;
 }
 
+- (void)playXPos:(int)x yPos:(int)y {
+  [board_ playXPos:x yPos:y toState:type_];
+}
+
 - (void)makeNextMove {
-  // Just make the first available.
   // TODO(cate): Do better.
+  // Fill in to make 3 if possible.
+  if ([self complete3IfPossibleForType:type_]) {
+    return;
+  }
+
+  // Block 3 if possible.
+  TicTacToeStateType userType = (type_ == TicTacToeStateO) ? TicTacToeStateX : TicTacToeStateO;
+  if ([self complete3IfPossibleForType:userType]) {
+    return;
+  }
+
+  // Just make the first available.
   for (int x = 0; x < 3; x++) {
     for (int y = 0; y < 3; y++) {
       if ([board_ stateForXPos:x yPos:y] == TicTacToeStateEmpty) {
-        [board_ playXPos:x yPos:y toState:type_];
+        [self playXPos:x yPos:y];
         return;
       }
     }
   }
+}
+
+- (BOOL)complete3IfPossibleForType:(TicTacToeStateType)type {
+  for (int i = 0; i < 3; i++) {
+    if ([self completeRowIfPossible:i type:type_]) {
+      return YES;
+    }
+    if ([self completeColumnIfPossible:i type:type_]) {
+      return YES;
+    }
+  }
+  // TODO(cate):Diagonals.
+  return NO;
+}
+
+- (BOOL)completeColumnIfPossible:(int)row type:(TicTacToeStateType)type {
+  TicTacToeStateType sqA = [board_ stateForXPos:0 yPos:row];
+  TicTacToeStateType sqB = [board_ stateForXPos:1 yPos:row];
+  TicTacToeStateType sqC = [board_ stateForXPos:2 yPos:row];
+
+  if ([self checkState:type forSquareA:sqA squareB:sqB squareC:sqC]) {
+    if (sqA == TicTacToeStateEmpty) {
+      [self playXPos:0 yPos:row];
+      return YES;
+    }
+    if (sqB == TicTacToeStateEmpty) {
+      [self playXPos:1 yPos:row];
+      return YES;
+    }
+    if (sqC == TicTacToeStateEmpty) {
+      [self playXPos:2 yPos:row];
+      return YES;
+    }
+  }
+  return NO;
+}
+
+- (BOOL)completeRowIfPossible:(int)row type:(TicTacToeStateType)type {
+  TicTacToeStateType sqA = [board_ stateForXPos:row yPos:0];
+  TicTacToeStateType sqB = [board_ stateForXPos:row yPos:1];
+  TicTacToeStateType sqC = [board_ stateForXPos:row yPos:2];
+
+  if ([self checkState:type forSquareA:sqA squareB:sqB squareC:sqC]) {
+    if (sqA == TicTacToeStateEmpty) {
+      [self playXPos:row yPos:0];
+      return YES;
+    }
+    if (sqB == TicTacToeStateEmpty) {
+      [self playXPos:row yPos:1];
+      return YES;
+    }
+    if (sqC == TicTacToeStateEmpty) {
+      [self playXPos:row yPos:1];
+      return YES;
+    }
+  }
+  return NO;
+}
+
+- (BOOL)checkState:(TicTacToeStateType)state
+        forSquareA:(TicTacToeStateType)sqA
+           squareB:(TicTacToeStateType)sqB
+           squareC:(TicTacToeStateType)sqC {
+  int empty = 0;
+  empty += (sqA == TicTacToeStateEmpty) ? 1 : 0;
+  empty += (sqB == TicTacToeStateEmpty) ? 1 : 0;
+  empty += (sqC == TicTacToeStateEmpty) ? 1 : 0;
+
+  // One should be empty.
+  if (empty != 1) {
+    return NO;
+  }
+
+  int ofState = 0;
+  ofState += (sqA == state) ? 1 : 0;
+  ofState += (sqB == state) ? 1 : 0;
+  ofState += (sqC == state) ? 1 : 0;
+
+  // The other two should be the right type.
+  return ofState == 2;
 }
 
 @end
--- TicTacToe/TicTacToeTests/model/ComputerPlayerTest.m
+++ TicTacToe/TicTacToeTests/model/ComputerPlayerTest.m
@@ -54,4 +54,112 @@
   XCTAssertEqual([self countFilledSquares], 9);
 }
 
+- (void)testCompleteRowOO_ {
+  [board_ playXPos:0 yPos:0 toState:TicTacToeStateO];
+  [board_ playXPos:1 yPos:0 toState:TicTacToeStateO];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:2 yPos:0], TicTacToeStateO);
+}
+
+- (void)testCompleteRowO_O {
+  [board_ playXPos:0 yPos:1 toState:TicTacToeStateO];
+  [board_ playXPos:2 yPos:1 toState:TicTacToeStateO];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:1 yPos:1], TicTacToeStateO);
+}
+
+- (void)testCompleteRow_OO {
+  [board_ playXPos:1 yPos:2 toState:TicTacToeStateO];
+  [board_ playXPos:2 yPos:2 toState:TicTacToeStateO];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:0 yPos:2], TicTacToeStateO);
+}
+
+- (void)testCompleteColumnOO_ {
+  [board_ playXPos:0 yPos:0 toState:TicTacToeStateO];
+  [board_ playXPos:0 yPos:1 toState:TicTacToeStateO];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:0 yPos:2], TicTacToeStateO);
+}
+
+- (void)testCompleteColumnO_O {
+  [board_ playXPos:1 yPos:0 toState:TicTacToeStateO];
+  [board_ playXPos:1 yPos:2 toState:TicTacToeStateO];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:1 yPos:1], TicTacToeStateO);
+}
+
+- (void)testCompleteColumn_OO {
+  [board_ playXPos:2 yPos:1 toState:TicTacToeStateO];
+  [board_ playXPos:2 yPos:2 toState:TicTacToeStateO];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:2 yPos:0], TicTacToeStateO);
+}
+
+- (void)testBlockCompleteRowXX_ {
+  [board_ playXPos:0 yPos:0 toState:TicTacToeStateX];
+  [board_ playXPos:1 yPos:0 toState:TicTacToeStateX];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:2 yPos:0], TicTacToeStateO);
+}
+
+- (void)testBlockCompleteRowX_X {
+  [board_ playXPos:0 yPos:1 toState:TicTacToeStateX];
+  [board_ playXPos:2 yPos:1 toState:TicTacToeStateX];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:1 yPos:1], TicTacToeStateO);
+}
+
+- (void)testBlockCompleteRow_XX {
+  [board_ playXPos:1 yPos:2 toState:TicTacToeStateX];
+  [board_ playXPos:2 yPos:2 toState:TicTacToeStateX];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:0 yPos:2], TicTacToeStateO);
+}
+
+- (void)testBlockCompleteColumnXX_ {
+  [board_ playXPos:0 yPos:0 toState:TicTacToeStateX];
+  [board_ playXPos:0 yPos:1 toState:TicTacToeStateX];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:0 yPos:2], TicTacToeStateO);
+}
+
+- (void)testBlockCompleteColumnX_X {
+  [board_ playXPos:1 yPos:0 toState:TicTacToeStateX];
+  [board_ playXPos:1 yPos:2 toState:TicTacToeStateX];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:1 yPos:1], TicTacToeStateO);
+}
+
+- (void)testBlockCompleteColumn_XX {
+  [board_ playXPos:2 yPos:1 toState:TicTacToeStateX];
+  [board_ playXPos:2 yPos:2 toState:TicTacToeStateX];
+
+  [player_ makeNextMove];
+
+  XCTAssertEqual([board_ stateForXPos:2 yPos:0], TicTacToeStateO);
+}
+
 @end
