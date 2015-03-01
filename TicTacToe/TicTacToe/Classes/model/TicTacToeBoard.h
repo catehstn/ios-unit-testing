573dd7cbb8688c613fa565c2a27e1f9da6560a42
Cate Huston <catehuston@gmail.com>
2015-03-01 13:06:14 +0100

    adding comments to board interface

--- TicTacToe/TicTacToe/Classes/model/TicTacToeBoard.h
+++ TicTacToe/TicTacToe/Classes/model/TicTacToeBoard.h
@@ -4,10 +4,13 @@
 
 @interface TicTacToeBoard : NSObject
 
+// Returns the state of the game: not ended, O wins, X wins, or tie.
 @property(nonatomic, readonly) TicTacToeGameStateType gameState;
 
+// Returns the state of the square at the given position on the board.
 - (TicTacToeStateType)stateForXPos:(int)x yPos:(int)y;
 
+// Attempts to play in the position (x,y). Returns YES for success, NO if not.
 - (BOOL)playXPos:(int)x
             yPos:(int)y
          toState:(TicTacToeStateType)state;
